0796455cb889b9a5b5b4bf47bb4aa7cba5344bc9
Alan Moran <bonzofenix@gmail.com>
2024-08-02 16:11:53 +0200

    Refactor CloudFoundryClientFactory imports and methods
     • Consolidate imports and remove unused ones
     • Refactor createClient, createLogCacheClient, createSpaceClient, and related methods for clarity
     • Update getOrCreateConnectionContext to always create a new context
     • Enhance SSL handling in getAdditionalHttpClientConfiguration with custom trust manager
     • Add logging statements for debugging purposes

--- src/main/java/com/sap/cloudfoundry/client/facade/adapters/CloudFoundryClientFactory.java
+++ src/main/java/com/sap/cloudfoundry/client/facade/adapters/CloudFoundryClientFactory.java
@@ -1,13 +1,20 @@
 package com.sap.cloudfoundry.client.facade.adapters;
 
+import com.sap.cloudfoundry.client.facade.CloudException;
+import com.sap.cloudfoundry.client.facade.CloudOperationException;
+import com.sap.cloudfoundry.client.facade.Messages;
+import com.sap.cloudfoundry.client.facade.oauth2.OAuthClient;
+import com.sap.cloudfoundry.client.facade.rest.CloudSpaceClient;
+import com.sap.cloudfoundry.client.facade.util.CloudUtil;
+import com.sap.cloudfoundry.client.facade.util.JsonUtil;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 import java.text.MessageFormat;
 import java.time.Duration;
 import java.util.Map;
@@ -15,8 +22,7 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.function.Function;
-
-import io.netty.handler.ssl.SslContextBuilder;
+import javax.net.ssl.SSLException;
 import org.cloudfoundry.client.CloudFoundryClient;
 import org.cloudfoundry.client.v3.organizations.OrganizationsV3;
 import org.cloudfoundry.client.v3.spaces.SpacesV3;
@@ -29,173 +35,179 @@ import org.immutables.value.Value;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-
-import com.sap.cloudfoundry.client.facade.CloudException;
-import com.sap.cloudfoundry.client.facade.CloudOperationException;
-import com.sap.cloudfoundry.client.facade.Messages;
-import com.sap.cloudfoundry.client.facade.oauth2.OAuthClient;
-import com.sap.cloudfoundry.client.facade.rest.CloudSpaceClient;
-import com.sap.cloudfoundry.client.facade.util.CloudUtil;
-import com.sap.cloudfoundry.client.facade.util.JsonUtil;
-
 import reactor.core.publisher.Mono;
-import reactor.netty.tcp.SslProvider;
-
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLException;
-import javax.net.ssl.TrustManager;
 
 @Value.Immutable
 public abstract class CloudFoundryClientFactory {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CloudFoundryClientFactory.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(CloudFoundryClientFactory.class);
 
-    static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
-                                                    .executor(Executors.newSingleThreadExecutor())
-                                                    .followRedirects(HttpClient.Redirect.NORMAL)
-                                                    .connectTimeout(Duration.ofMinutes(10))
-                                                    .build();
+  static final HttpClient HTTP_CLIENT =
+      HttpClient.newBuilder()
+          .executor(Executors.newSingleThreadExecutor())
+          .followRedirects(HttpClient.Redirect.NORMAL)
+          .connectTimeout(Duration.ofMinutes(10))
+          .build();
 
-    private final Map<String, ConnectionContext> connectionContextCache = new ConcurrentHashMap<>();
+  private final Map<String, ConnectionContext> connectionContextCache = new ConcurrentHashMap<>();
 
-    public abstract Optional<Duration> getSslHandshakeTimeout();
+  public abstract Optional<Duration> getSslHandshakeTimeout();
 
-    public abstract Optional<Duration> getConnectTimeout();
+  public abstract Optional<Duration> getConnectTimeout();
 
-    public abstract Optional<Integer> getConnectionPoolSize();
+  public abstract Optional<Integer> getConnectionPoolSize();
 
-    public abstract Optional<Integer> getThreadPoolSize();
+  public abstract Optional<Integer> getThreadPoolSize();
 
-    public abstract Optional<Duration> getResponseTimeout();
+  public abstract Optional<Duration> getResponseTimeout();
 
-    public CloudFoundryClient createClient(URL controllerUrl, OAuthClient oAuthClient, Map<String, String> requestTags) {
-        return ReactorCloudFoundryClient.builder()
-                                        .connectionContext(getOrCreateConnectionContext(controllerUrl.getHost()))
-                                        .tokenProvider(oAuthClient.getTokenProvider())
-                                        .requestTags(requestTags)
-                                        .build();
+  public CloudFoundryClient createClient(
+      URL controllerUrl, OAuthClient oAuthClient, Map<String, String> requestTags) {
+    return ReactorCloudFoundryClient.builder()
+        .connectionContext(getOrCreateConnectionContext(controllerUrl.getHost()))
+        .tokenProvider(oAuthClient.getTokenProvider())
+        .requestTags(requestTags)
+        .build();
+  }
+
+  public LogCacheClient createLogCacheClient(
+      URL controllerUrl, OAuthClient oAuthClient, Map<String, String> requestTags) {
+    String logCacheApi;
+    try {
+      var links = CloudUtil.executeWithRetry(() -> callCfRoot(controllerUrl, requestTags));
+      @SuppressWarnings("unchecked")
+      var logCache = (Map<String, Object>) links.get("log_cache");
+      logCacheApi = (String) logCache.get("href");
+    } catch (CloudException e) {
+      LOGGER.warn(
+          MessageFormat.format(
+              Messages.CALL_TO_0_FAILED_WITH_1, controllerUrl.toString(), e.getMessage()),
+          e);
+      logCacheApi = controllerUrl.toString().replace("api", "log-cache");
     }
+    return new LogCacheClient(logCacheApi, oAuthClient, requestTags);
+  }
 
-    public LogCacheClient createLogCacheClient(URL controllerUrl, OAuthClient oAuthClient, Map<String, String> requestTags) {
-        String logCacheApi;
-        try {
-            var links = CloudUtil.executeWithRetry(() -> callCfRoot(controllerUrl, requestTags));
-            @SuppressWarnings("unchecked")
-            var logCache = (Map<String, Object>) links.get("log_cache");
-            logCacheApi = (String) logCache.get("href");
-        } catch (CloudException e) {
-            LOGGER.warn(MessageFormat.format(Messages.CALL_TO_0_FAILED_WITH_1, controllerUrl.toString(), e.getMessage()), e);
-            logCacheApi = controllerUrl.toString()
-                                       .replace("api", "log-cache");
-        }
-        return new LogCacheClient(logCacheApi, oAuthClient, requestTags);
+  @SuppressWarnings("unchecked")
+  private Map<String, Object> callCfRoot(URL controllerUrl, Map<String, String> requestTags) {
+    HttpResponse<String> response;
+    try {
+      HttpRequest request = buildCfRootRequest(controllerUrl, requestTags);
+      LOGGER.info(
+          MessageFormat.format(Messages.CALLING_CF_ROOT_0_TO_ACCESS_LOG_CACHE_URL, controllerUrl));
+      response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+      if (response.statusCode() / 100 != 2) {
+        var status = HttpStatus.valueOf(response.statusCode());
+        throw new CloudOperationException(status, status.getReasonPhrase(), response.body());
+      }
+      LOGGER.info(Messages.CF_ROOT_REQUEST_FINISHED);
+    } catch (InterruptedException | URISyntaxException | IOException e) {
+      throw new CloudException(e.getMessage(), e);
     }
+    var map = JsonUtil.convertJsonToMap(response.body());
+    return (Map<String, Object>) map.get("links");
+  }
 
-    @SuppressWarnings("unchecked")
-    private Map<String, Object> callCfRoot(URL controllerUrl, Map<String, String> requestTags) {
-        HttpResponse<String> response;
-        try {
-            HttpRequest request = buildCfRootRequest(controllerUrl, requestTags);
-            LOGGER.info(MessageFormat.format(Messages.CALLING_CF_ROOT_0_TO_ACCESS_LOG_CACHE_URL, controllerUrl));
-            response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() / 100 != 2) {
-                var status = HttpStatus.valueOf(response.statusCode());
-                throw new CloudOperationException(status, status.getReasonPhrase(), response.body());
-            }
-            LOGGER.info(Messages.CF_ROOT_REQUEST_FINISHED);
-        } catch (InterruptedException | URISyntaxException | IOException e) {
-            throw new CloudException(e.getMessage(), e);
-        }
-        var map = JsonUtil.convertJsonToMap(response.body());
-        return (Map<String, Object>) map.get("links");
+  private HttpRequest buildCfRootRequest(URL controllerUrl, Map<String, String> requestTags)
+      throws URISyntaxException {
+    var requestBuilder =
+        HttpRequest.newBuilder().GET().uri(controllerUrl.toURI()).timeout(Duration.ofMinutes(5));
+    requestTags.forEach(requestBuilder::header);
+    return requestBuilder.build();
+  }
+
+  public CloudSpaceClient createSpaceClient(
+      URL controllerUrl, OAuthClient oAuthClient, Map<String, String> requestTags) {
+    String v3Api;
+    try {
+      var links = CloudUtil.executeWithRetry(() -> callCfRoot(controllerUrl, requestTags));
+      @SuppressWarnings("unchecked")
+      var ccv3 = (Map<String, Object>) links.get("cloud_controller_v3");
+      v3Api = (String) ccv3.get("href");
+    } catch (CloudException e) {
+      LOGGER.warn(
+          MessageFormat.format(
+              Messages.CALL_TO_0_FAILED_WITH_1, controllerUrl.toString(), e.getMessage()),
+          e);
+      v3Api = controllerUrl + "/v3";
     }
+    LOGGER.info("custom-test: calling createV3SpacesClient");
+    var spacesV3 = createV3SpacesClient(controllerUrl, v3Api, oAuthClient, requestTags);
+    var orgsV3 = createV3OrgsClient(controllerUrl, v3Api, oAuthClient, requestTags);
+    return new CloudSpaceClient(spacesV3, orgsV3);
+  }
 
-    private HttpRequest buildCfRootRequest(URL controllerUrl, Map<String, String> requestTags) throws URISyntaxException {
-        var requestBuilder = HttpRequest.newBuilder()
-                                        .GET()
-                                        .uri(controllerUrl.toURI())
-                                        .timeout(Duration.ofMinutes(5));
-        requestTags.forEach(requestBuilder::header);
-        return requestBuilder.build();
+  private SpacesV3 createV3SpacesClient(
+      URL controllerUrl, String v3Api, OAuthClient oAuthClient, Map<String, String> requestTags) {
+    return new ReactorSpacesV3(
+        getOrCreateConnectionContext(controllerUrl.getHost()),
+        Mono.just(v3Api),
+        oAuthClient.getTokenProvider(),
+        requestTags);
+  }
+
+  private OrganizationsV3 createV3OrgsClient(
+      URL controllerUrl, String v3Api, OAuthClient oAuthClient, Map<String, String> requestTags) {
+    return new ReactorOrganizationsV3(
+        getOrCreateConnectionContext(controllerUrl.getHost()),
+        Mono.just(v3Api),
+        oAuthClient.getTokenProvider(),
+        requestTags);
+  }
+
+  public ConnectionContext getOrCreateConnectionContext(String controllerApiHost) {
+    LOGGER.info("custom-test: calling createConnectionContext");
+    LOGGER.info("custom-test: checking connectionContextCache" + connectionContextCache);
+    return createConnectionContext(controllerApiHost); // create connection context everytime
+    // return connectionContextCache.computeIfAbsent(controllerApiHost,
+    // this::createConnectionContext);
+  }
+
+  private ConnectionContext createConnectionContext(String controllerApiHost) {
+    LOGGER.info("custom-test: inside createConnectionContext");
+    DefaultConnectionContext.Builder builder =
+        DefaultConnectionContext.builder().skipSslValidation(true).apiHost(controllerApiHost);
+    LOGGER.info("custom-test: connection skipSslValidation");
+    getSslHandshakeTimeout().ifPresent(builder::sslHandshakeTimeout);
+    getConnectTimeout().ifPresent(builder::connectTimeout);
+    getConnectionPoolSize().ifPresent(builder::connectionPoolSize);
+    getThreadPoolSize().ifPresent(builder::threadPoolSize);
+    LOGGER.info("custom-test: connection before getAdditionalHttpClientConfiguration");
+    builder.additionalHttpClientConfiguration(this::getAdditionalHttpClientConfiguration);
+    LOGGER.info("custom-test: connection after getAdditionalHttpClientConfiguration");
+    DefaultConnectionContext build = builder.build();
+    LOGGER.info("custom-test: created connection context" + build);
+    return build;
+  }
+
+  private reactor.netty.http.client.HttpClient getAdditionalHttpClientConfiguration(
+      reactor.netty.http.client.HttpClient client) {
+    var clientWithOptions = client;
+    if (getResponseTimeout().isPresent()) {
+      clientWithOptions = clientWithOptions.responseTimeout(getResponseTimeout().get());
     }
-
-    public CloudSpaceClient createSpaceClient(URL controllerUrl, OAuthClient oAuthClient, Map<String, String> requestTags) {
-        String v3Api;
-        try {
-            var links = CloudUtil.executeWithRetry(() -> callCfRoot(controllerUrl, requestTags));
-            @SuppressWarnings("unchecked")
-            var ccv3 = (Map<String, Object>) links.get("cloud_controller_v3");
-            v3Api = (String) ccv3.get("href");
-        } catch (CloudException e) {
-            LOGGER.warn(MessageFormat.format(Messages.CALL_TO_0_FAILED_WITH_1, controllerUrl.toString(), e.getMessage()), e);
-            v3Api = controllerUrl + "/v3";
-        }
-        LOGGER.info("custom-test: calling createV3SpacesClient");
-        var spacesV3 = createV3SpacesClient(controllerUrl, v3Api, oAuthClient, requestTags);
-        var orgsV3 = createV3OrgsClient(controllerUrl, v3Api, oAuthClient, requestTags);
-        return new CloudSpaceClient(spacesV3, orgsV3);
-    }
-
-    private SpacesV3 createV3SpacesClient(URL controllerUrl, String v3Api, OAuthClient oAuthClient, Map<String, String> requestTags) {
-        return new ReactorSpacesV3(getOrCreateConnectionContext(controllerUrl.getHost()),
-                                   Mono.just(v3Api),
-                                   oAuthClient.getTokenProvider(),
-                                   requestTags);
-    }
-
-    private OrganizationsV3 createV3OrgsClient(URL controllerUrl, String v3Api, OAuthClient oAuthClient, Map<String, String> requestTags) {
-        return new ReactorOrganizationsV3(getOrCreateConnectionContext(controllerUrl.getHost()),
-                                          Mono.just(v3Api),
-                                          oAuthClient.getTokenProvider(),
-                                          requestTags);
-    }
-
-    public ConnectionContext getOrCreateConnectionContext(String controllerApiHost) {
-        LOGGER.info("custom-test: calling createConnectionContext");
-        LOGGER.info("custom-test: checking connectionContextCache" + connectionContextCache);
-        return createConnectionContext(controllerApiHost); // create connection context everytime
-        //return connectionContextCache.computeIfAbsent(controllerApiHost, this::createConnectionContext);
-    }
-
-    private ConnectionContext createConnectionContext(String controllerApiHost) {
-        LOGGER.info("custom-test: inside createConnectionContext");
-        DefaultConnectionContext.Builder builder = DefaultConnectionContext.builder()
-                .skipSslValidation(true)
-                .apiHost(controllerApiHost);
-        LOGGER.info("custom-test: connection skipSslValidation");
-        getSslHandshakeTimeout().ifPresent(builder::sslHandshakeTimeout);
-        getConnectTimeout().ifPresent(builder::connectTimeout);
-        getConnectionPoolSize().ifPresent(builder::connectionPoolSize);
-        getThreadPoolSize().ifPresent(builder::threadPoolSize);
-        LOGGER.info("custom-test: connection before getAdditionalHttpClientConfiguration");
-        builder.additionalHttpClientConfiguration(this::getAdditionalHttpClientConfiguration);
-        LOGGER.info("custom-test: connection after getAdditionalHttpClientConfiguration");
-        DefaultConnectionContext build = builder.build();
-        LOGGER.info("custom-test: created connection context" + build);
-        return build;
-    }
-
-    private reactor.netty.http.client.HttpClient getAdditionalHttpClientConfiguration(reactor.netty.http.client.HttpClient client) {
-        var clientWithOptions = client;
-        if (getResponseTimeout().isPresent()) {
-            clientWithOptions = clientWithOptions.responseTimeout(getResponseTimeout().get());
-        }
-        clientWithOptions = clientWithOptions.metrics(true, Function.identity());
-        LOGGER.info("custom-test: before getAdditionalHttpClientConfiguration");
-        // TODO: just commect the next line
-        //clientWithOptions = clientWithOptions.noSSL();
-        FascadeSSLUtil.disableSSLValidation();
-        clientWithOptions.secure( sslContextSpec -> {
-            try {
-                SslContextBuilder.forClient().trustManager(FascadeSSLUtil.NULL_TRUST_MANAGER).build();
-            } catch (SSLException e) {
+    clientWithOptions = clientWithOptions.metrics(true, Function.identity());
+    LOGGER.info("custom-test: before getAdditionalHttpClientConfiguration");
+    // TODO: just commect the next line
+    // clientWithOptions = clientWithOptions.noSSL();
+    FascadeSSLUtil.disableSSLValidation();
+    clientWithOptions =
+        clientWithOptions.secure(
+            t -> {
+              final SslContext sslContext;
+              try {
+                sslContext =
+                    SslContextBuilder.forClient()
+                        .trustManager(FascadeSSLUtil.NULL_TRUST_MANAGER)
+                        .build();
+                t.sslContext(sslContext);
+              } catch (SSLException e) {
+                LOGGER.info("custom-test: error creatign ssl context", e);
                 throw new RuntimeException(e);
-            }
-        });
-        LOGGER.info("custom-test: after getAdditionalHttpClientConfiguration");
+              }
+            });
+    LOGGER.info("custom-test: after getAdditionalHttpClientConfiguration");
 
-        return clientWithOptions;
-    }
-
+    return clientWithOptions;
+  }
 }
